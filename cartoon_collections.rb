e5a1e08b3a5314e3207d646cc19d7e197fb7d71b
Orlando Challenger <orlandofat@gmail.com>
2020-05-22 02:35:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,5 +31,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find {|s| 
-     s == valid_calls.include?()}
+     valid_calls.include?()}
 end
